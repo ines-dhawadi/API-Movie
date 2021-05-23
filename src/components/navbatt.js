1c61f489b3283d8b8c8dde62b7811a09081d7ae7
ines-dhawadi <inesdhawadisimplonend@gmail.com>
2021-05-23 04:59:12 +0100

    dashbord

--- src/components/navbatt.js
+++ src/components/navbatt.js
@@ -21,10 +21,20 @@ function Nnabarr({handelSearch,favorites}) {
        <Link to="/favored"><img src="./heart.png" /> <span className="val-favorite"> {favorites}</span></Link>
       
     </Nav>
-    <Form inline>
+    {/* <Form inline>
       <FormControl type="text" placeholder="Search" className="mr-sm-2"  onChange={handelSearch} />
       <Button variant="outline-danger"  >Search</Button>
-    </Form>
+    </Form> */}
+
+    <form class="navbar-form">
+              <div class="input-group no-border">
+                <input type="text" value="" class="form-control" onChange={handelSearch}  placeholder="Search..."/>
+                <button type="submit" class="btn btn-default btn-round btn-just-icon">
+                  <i class="material-icons">search</i>
+                  <div class="ripple-container"></div>
+                </button>
+              </div>
+            </form>
   </Navbar.Collapse>
 </Navbar>
 
